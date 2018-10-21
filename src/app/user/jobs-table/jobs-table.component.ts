e9d559ba97e806e8683e460508898a59da95e785
eliserothberg <rothberg.elise@gmail.com>
2018-10-21 12:03:12 -0700

    minor changes

---
+++ src/app/user/jobs-table/jobs-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jobs-table',
+  templateUrl: './jobs-table.component.html',
+  styleUrls: ['./jobs-table.component.css']
+})
+export class JobsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user/sign-up-manager/experience-and-interests-form/experience-and-interests-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-and-interests-form',
+  templateUrl: './experience-and-interests-form.component.html',
+  styleUrls: ['./experience-and-interests-form.component.css']
+})
+export class ExperienceAndInterestsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
